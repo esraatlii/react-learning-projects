1de1aeda5a45b0579bf070e53fb255ed5879c226
Esra Atli <esraaatli2004@gmail.com>
2026-07-05 16:29:34 +0300

    İlk commit

---
+++ 7.course-app/src/Data.jsx
@@ -0,0 +1,39 @@
+import Csharp from "./images/csharp.jpg";
+import java from "./images/java.jpg";
+import python from './images/python.jpg';
+import web from './images/web.jpg';
+
+export const courses = [
+    {
+        id: 1268,
+        title: "C# Programlama Dili",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta corrupti consequatur deleniti repellendus quis nisi commodi pariatur assumenda officiis consequuntur dolores fugiat, quae itaque amet! Corrupti sit ut rem blanditiis?",
+        price: 400,
+        image: Csharp,
+        link: "satın almak için tıklayın"
+    },
+    {
+        id: 1269,
+        title: "Java Programlama Dili",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta corrupti consequatur deleniti repellendus quis nisi commodi pariatur assumenda officiis consequuntur dolores fugiat, quae itaque amet! Corrupti sit ut rem blanditiis?",
+        price: 420,
+        image: java,
+        link: "satın almak için tıklayın"
+    },
+    {
+        id: 1270,
+        title: "Python Programlama Dili",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta corrupti consequatur deleniti repellendus quis nisi commodi pariatur assumenda officiis consequuntur dolores fugiat, quae itaque amet! Corrupti sit ut rem blanditiis?",
+        price: 500,
+        image: python,
+        link: "satın almak için tıklayın"
+    },
+    {
+        id: 1271,
+        title: "web Programlama kursu",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta corrupti consequatur deleniti repellendus quis nisi commodi pariatur assumenda officiis consequuntur dolores fugiat, quae itaque amet! Corrupti sit ut rem blanditiis?",
+        price: 300,
+        image: web,
+        link: "satın almak için tıklayın"
+    },
+]
